471bd520bdaa3610aaf0f77562befe5c036d7406
zhangliang <17602530732@163.com>
2021-03-29 00:14:35 +0800

    测试git在idea的使用

---
+++ src/main/java/com/gupao/edu/git/demo/App.java
@@ -0,0 +1,13 @@
+package com.gupao.edu.git.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
