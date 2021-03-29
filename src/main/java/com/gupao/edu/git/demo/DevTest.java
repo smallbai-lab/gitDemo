122f03e0f6f0fb0a0da62225eb42257da3dc0688
zhangliang <17602530732@163.com>
2021-03-29 14:08:14 +0800

    dev test

---
+++ src/main/java/com/gupao/edu/git/demo/DevTest.java
@@ -0,0 +1,15 @@
+package com.gupao.edu.git.demo;
+
+/**
+ * @Program git-demo
+ * @ClassName DevTest
+ * @Description: TODO
+ * @Author: zhangliang
+ * @Email: 17602530732@163.com
+ * @Date: 2021/03/29 14:07
+ */
+public class DevTest {
+    public static void main(String[] args) {
+        System.out.println("dev test");
+    }
+}
